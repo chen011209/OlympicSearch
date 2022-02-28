7a5add2a984166a4a847418cbee448373fc64c28
cyc <1356985444@qq.com>
2022-02-28 20:55:01 +0800

    第一次优化

--- 061900204/src/OlympicSearch.java
+++ 061900204/src/OlympicSearch.java
@@ -3,6 +3,28 @@ import java.util.ArrayList;
 
 public class OlympicSearch {
     public static void main(String[] args) {
+
+        long startTime=System.currentTimeMillis(); //获取开始时间
+
+
+
+        //去掉文件末尾的换行
+        //多种输入文件格式
+
+        //性能
+        //改进前
+        //4000 平均3000ms
+        //性能
+        //改进后
+        //4000 平均1100ms
+
+
+
+        //一开始的思路 每次都访问文件 再输出文件
+        //第一次优化 访问一次文件后就在内存保存 再输出文件
+        //第二次优化 根据输出文件只记住需要输出的内容 最后一次性输出
+
+
         if(args.length!=2) {
             System.out.println("运行格式异常，正确格式需要加上输入文件和输出文件");
             return;
@@ -30,6 +52,14 @@ public class OlympicSearch {
         }
 
         new OutputData(requestType,args[1]);
+
+
+
+
+        long endTime=System.currentTimeMillis(); //获取结束时间
+
+        System.out.println("程序运行时间： "+(endTime-startTime)+"ms");
+
 
     }
 
--- 061900204/src/OutputData.java
+++ 061900204/src/OutputData.java
@@ -3,16 +3,34 @@ import bean.schedule.scheduleFile;
 import com.google.gson.Gson;
 import util.file;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
 import static util.file.isFileExist;
 import static util.file.isFolderExist;
 
 //该类根据输入文件输出对应的数据到输出文件中
 public class OutputData {
     String outputFile;
+    boolean []isOut=new boolean[20];
+    String []fileString =new String[20];
+    ArrayList outputCode;                       //输出的字符串代号    0为total 1到19为对应日期 2到20
+    Pattern pattern = Pattern.compile("^[-\\+]?[\\d]*$");
+    //判断字符串是否为数字的正则表达式
+
 
     //如果输出失败重新爬取一遍到文件后再输出
     public void outputTotal()
     {
+        if(isOut[0]==true)
+        {
+            file.writeFile(outputFile,fileString[0]);
+            return;
+        }
+
+
+
+
         Gson gson = new Gson();
         String jsonString = file.readFile("data/total.json");
         String outString = null;
@@ -36,11 +54,27 @@ public class OutputData {
         }
 
         file.writeFile(outputFile,outString);
+
+        isOut[0]=true;
+        fileString[0]=outString;
     }
 
 
     public void outputScedule(String date)
     {
+
+
+        int dateInt=Integer.parseInt(date);
+        if(isOut[dateInt-201]==true)
+        {
+            file.writeFile(outputFile,fileString[dateInt-201]);
+            return;
+        }
+
+
+
+
+
         Gson gson = new Gson();
         String jsonString = file.readFile("data/schedule/"+date+".json");
         String outString = null;
@@ -65,6 +99,11 @@ public class OutputData {
         }
 
         file.writeFile(outputFile,outString);
+
+
+
+        isOut[dateInt-201]=true;
+        fileString[dateInt-201]=outString;
     }
 
 
@@ -77,9 +116,27 @@ public class OutputData {
         isFolderExist("data");
         isFolderExist("data/schedule");
 
+
+
+
         for (String s:requestType)
         {
-            if(s.equals("total")) {
+
+
+
+            String[] sArray = s.split("\\s+"); //分割一个或者多个空格
+
+            //第一个可能是空字符串
+            if(sArray[0].length()==0){
+                sArray= Arrays.copyOfRange(sArray,1,sArray.length);
+            }
+
+
+
+
+
+
+            if(sArray.length==1&&sArray[0].equals("total")) {
                 //文件夹中不存在total.json文件则爬取数据,isFileExist函数中创建对应文件
                 if(!isFileExist("data/total.json")){
                     //getData函数在爬取成功后返回true在爬取失败后返回false
@@ -91,13 +148,22 @@ public class OutputData {
 
                 outputTotal();
 
-            }else if (s.length()==13&& s.startsWith("schedule")){
-                String date=s.substring(9,13);
+            }else if (sArray.length==2&&sArray[0].equals("schedule")){
+                String date=sArray[1];
+
+
+
+
+
+
+
+
 
                 //冬奥会时间为2月2日到2月20日
-                if(202<=Integer.parseInt(date)&&Integer.parseInt(date)<=220)
+                //第一个判断字符串是否是数字
+                if(pattern.matcher(date).matches()&&202<=Integer.parseInt(date)&&Integer.parseInt(date)<=220)
                 {
-                    if(!isFileExist("data/schedule/"+s.substring(9,13)+".json")){
+                    if(!isFileExist("data/schedule/"+sArray[1]+".json")){
                         //getData函数在爬取成功后返回true在爬取失败后返回false
                         if(!getData.getFile(date)){
                             System.out.println("爬取数据失败");
